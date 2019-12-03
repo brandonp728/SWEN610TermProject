9f3d3f7bfa593bd57c64762c4671c215d3c8bb49
= <bpalonis59@gmail.com>
2019-12-03 14:40:32 -0500

    Got db connected and integrated

---
+++ src/main/java/com/webcheckers/model/db-create.sql
@@ -0,0 +1,38 @@
+DROP TABLE IF EXISTS PLAYER CASCADE;
+DROP TABLE IF EXISTS ACCOUNT CASCADE;
+DROP TABLE IF EXISTS GAME CASCADE;
+DROP TABLE IF EXISTS TOURNAMENT CASCADE;
+
+CREATE TABLE PLAYER (
+    player_id SERIAL PRIMARY KEY,
+    f_name text,
+    l_name text,
+    is_computer bit,
+    games_won int,
+    games_lost int
+);
+
+CREATE TABLE ACCOUNT (
+    account_id SERIAL PRIMARY KEY,
+    username text,
+    pwd_hash text,
+    player_id int REFERENCES PLAYER(player_id)
+);
+
+CREATE TABLE TOURNAMENT(
+    tournament_id SERIAL PRIMARY KEY,
+    tournament_timestamp timestamp,
+    num_players int,
+    num_games int,
+    winner_id int REFERENCES PLAYER(player_id) 
+);
+
+CREATE TABLE GAME (
+    game_id SERIAL PRIMARY KEY,
+    elapsed_time timestamp,
+    game_timestamp timestamp,
+    player1_id int REFERENCES PLAYER(player_id),
+    player2_id int REFERENCES PLAYER(player_id),
+    winner_id int REFERENCES PLAYER(player_id),
+    tournament_id int REFERENCES TOURNAMENT(tournament_id)
+);
